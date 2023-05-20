798c85a8f5fba02143f1198faa373fb147c92f48
Juan Nobert <nobert407@gmail.com>
2023-05-20 17:06:26 -0300

    Projeto criado

---
+++ src/main/java/br/com/iftech/PontoGoApplication.java
@@ -0,0 +1,13 @@
+package br.com.iftech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/iftech/PontoGoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.iftech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
