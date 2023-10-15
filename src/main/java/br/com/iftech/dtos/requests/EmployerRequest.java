4985677315172e6b6c58260379ceebc310fb0258
Juan Nobert <nobert407@gmail.com>
2023-10-15 12:29:45 -0300

    Correção na validção do cpf e cnpj

--- src/main/java/br/com/iftech/dtos/requests/EmployerRequest.java
+++ src/main/java/br/com/iftech/dtos/requests/EmployerRequest.java
@@ -1,5 +1,6 @@
 package br.com.iftech.dtos.requests;
 
+import br.com.iftech.validations.anotations.CpfOrCnpj;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -16,10 +17,7 @@ public class EmployerRequest extends UserRequest{
 	
 	@NotNull
 	@NotBlank
-	@Pattern(
-			regexp = "([0-9]{2}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[\\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[-]?[0-9]{2})",
-			message = "CPF ou CNPJ inválidos"
-	)
+	@CpfOrCnpj
 	private String cpfCnpj;
 	
 	@NotNull
--- src/main/java/br/com/iftech/validations/anotations/CpfOrCnpjValidator.java
+++ src/main/java/br/com/iftech/validations/anotations/CpfOrCnpjValidator.java
@@ -4,6 +4,8 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class CpfOrCnpjValidator implements ConstraintValidator<CpfOrCnpj, String> {
+	private static final int[] pesoCPF = {11, 10, 9, 8, 7, 6, 5, 4, 3, 2};
+    private static final int[] pesoCNPJ = {6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
 	@Override
 	public boolean isValid(String value, ConstraintValidatorContext context) {
 		if (value == null) {
@@ -27,11 +29,39 @@ public class CpfOrCnpjValidator implements ConstraintValidator<CpfOrCnpj, String
 
 	private boolean validarCPF(String cpf) {
 		
-		return cpf.matches("^\\d{3}\\x2E\\d{3}\\x2E\\d{3}\\x2D\\d{2}$");
+		cpf = cpf.trim().replace(".", "").replace("-", "");
+        if ((cpf==null) || (cpf.length()!=11)) return false;
+
+        for (int j = 0; j < 10; j++)
+            if (padLeft(Integer.toString(j), Character.forDigit(j, 10)).equals(cpf))
+                return false;
+
+        Integer digito1 = calcularDigito(cpf.substring(0,9), pesoCPF);
+        Integer digito2 = calcularDigito(cpf.substring(0,9) + digito1, pesoCPF);
+        return cpf.equals(cpf.substring(0,9) + digito1.toString() + digito2.toString());
 	}
 	
 	private boolean validarCNPJ(String cnpj) {
 		
-		return cnpj.equals("^\\d{2}.\\d{3}.\\d{3}/\\d{4}-\\d{2}$");
+		cnpj = cnpj.trim().replace(".", "").replace("-", "");
+        if ((cnpj==null)||(cnpj.length()!=14)) return false;
+
+        Integer digito1 = calcularDigito(cnpj.substring(0,12), pesoCNPJ);
+        Integer digito2 = calcularDigito(cnpj.substring(0,12) + digito1, pesoCNPJ);
+        return cnpj.equals(cnpj.substring(0,12) + digito1.toString() + digito2.toString());
 	}
+	
+	private static int calcularDigito(String str, int[] peso) {
+        int soma = 0;
+        for (int indice=str.length()-1, digito; indice >= 0; indice-- ) {
+            digito = Integer.parseInt(str.substring(indice,indice+1));
+            soma += digito*peso[peso.length-str.length()+indice];
+        }
+        soma = 11 - soma % 11;
+        return soma > 9 ? 0 : soma;
+    }
+
+    private static String padLeft(String text, char character) {
+        return String.format("%11s", text).replace(' ', character);
+    }
 }
